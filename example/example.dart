11558d989f0bf29a13c840604720c2fc725e9052
raihansikdar <raihansikdar10@gmail.com>
2025-06-22 13:16:55 +0600

    add dart doc comment and example

---
+++ example/example.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_debug_logger/flutter_debug_logger.dart';
+
+void main() {
+  final mockApiResponse = {
+    "status": "success",
+    "data": {
+      "message": "Welcome to Flutter Debug Logger!",
+      "user": {"id": 1, "name": "Raihan Sikdar"}
+    }
+  };
+
+  FlutterDebugLogger.printJsonResponse(
+    url: "https://api.example.com/user/info",
+    method: "GET",
+    tag: "UserAPI",
+    statusCode: 200,
+    responseBody: mockApiResponse,
+  );
+}
--- lib/src/flutter_debug_logger.dart
+++ lib/src/flutter_debug_logger.dart
@@ -1,10 +1,34 @@
 import 'dart:convert';
 import 'dart:developer';
 
+/// A utility class for logging formatted API response data in a clean,
+/// Postman-like style using the built-in `log()` function.
 class FlutterDebugLogger {
+  // Private constructor to prevent instantiation.
   FlutterDebugLogger._();
 
-  static printJsonResponse({
+  /// Logs a pretty-printed JSON API response to the console.
+  ///
+  /// This method prints the API URL, HTTP method, status code,
+  /// and the formatted response body for debugging purposes.
+  ///
+  /// Example output:
+  /// ```
+  /// ----->[MyTag] URL         ==> https://example.com/api
+  /// ----->[MyTag] Method      ==> GET
+  /// ----->[MyTag] StatusCode  ==> 200
+  /// ----->[MyTag] Body        ==> {
+  ///   "message": "Success"
+  /// }
+  /// ```
+  ///
+  /// - [url]: The full API URL.
+  /// - [method]: HTTP method used (e.g., 'GET', 'POST').
+  /// - [tag]: Custom tag to identify the log block.
+  /// - [statusCode]: The HTTP response code.
+  /// - [responseBody]: The response data (can be String, Map, or List).
+  
+  static void printJsonResponse({
     required String url,
     String method = 'N/A',
     required String tag,
@@ -21,7 +45,8 @@ class FlutterDebugLogger {
         final decoded = jsonDecode(responseBody);
         prettyJson = encoder.convert(decoded);
       } else {
-        prettyJson ='\n-----> Unsupported response body type: ${responseBody.runtimeType}';
+        prettyJson =
+            '\n----->> Unsupported response body type: ${responseBody.runtimeType}';
       }
 
       if (url.isNotEmpty && url != 'N/A') {
